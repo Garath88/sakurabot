7d73407503d652414449d8d88b92219cad66a022
Garath <garath88@gmail.com>
2018-11-14 23:12:50 +0100

    - Updated media patterns

--- src/main/java/com/sakura/bot/commands/copy/MediaPatterns.java
+++ src/main/java/com/sakura/bot/commands/copy/MediaPatterns.java
@@ -13,8 +13,10 @@ final class MediaPatterns {
         Pattern.compile("^http(s?)://.*fc2.*");
     private static final Pattern URL_BOORU_PATTERN =
         Pattern.compile("^http(s?)://.*booru.*");
-    private static final Pattern URL_DISCORD_PATTERN =
+    private static final Pattern URL_CDN_DISCORD_PATTERN =
         Pattern.compile("^http(s?)://cdn.discordapp.com/.*");
+    private static final Pattern URL_MEDIA_DISCORD_PATTERN =
+        Pattern.compile("^http(s?)://media.discordapp.net/.*");
     private static final Pattern URL_WEBMSHARE_PATTERN =
         Pattern.compile("^http(s?)://.*webmshare.com.*");
     private static final Pattern URL_IMGUR_SINGLE_PATTERN =
@@ -23,17 +25,21 @@ final class MediaPatterns {
         Pattern.compile("http(s?)://imgur\\.com/(a/|gallery/|r/[^/]+/)[A-Za-z0-9]+(#[A-Za-z0-9]+)?$");
     private static final Pattern URL_GFYCAT_PATTERN =
         Pattern.compile("^http(s?)://gfycat\\.com/(gifs/detail/)?[A-Za-z]+$");
+    private static final Pattern URL_NOZOMI_PATTERN =
+        Pattern.compile("^http(s?)://i.nozomi.la");
 
     private static final List<Pattern> PATTERNS = Arrays.asList(
         URL_TWITTER_PATTERN,
         URL_TWITTER_STATUS_PATTERN,
         URL_FC2_PATTERN,
         URL_BOORU_PATTERN,
-        URL_DISCORD_PATTERN,
+        URL_CDN_DISCORD_PATTERN,
+        URL_MEDIA_DISCORD_PATTERN,
         URL_WEBMSHARE_PATTERN,
         URL_IMGUR_SINGLE_PATTERN,
         URL_IMGUR_ALBUM_PATTERN,
-        URL_GFYCAT_PATTERN);
+        URL_GFYCAT_PATTERN,
+        URL_NOZOMI_PATTERN);
 
     private MediaPatterns() {
     }
